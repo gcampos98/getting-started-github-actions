21d314fddd33d77f9a9019bfe976cd03f49bc57f
gcampos98 <52362129+gcampos98@users.noreply.github.com>
2024-02-03 23:18:43 -0300

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,17 @@
+echo 1. What is in this directory?
+ls -la
+echo
+echo 2. Is Java installed?
+java -version
+echo
+echo 3. Is Git installed?
+git --version
+echo
+echo 4. What is my OS release?
+cat /etc/os-release
+echo
+echo 5. How is the disk laid out?
+df -h
+echo
+echo 6. What envieronment variables are available?
+env
